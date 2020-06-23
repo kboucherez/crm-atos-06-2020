07b1863cb4a9224fc73ae888a01c854225579086
BOUCHEREZ <kevin.boucherez@atos.net>
2020-06-23 15:42:13 +0200

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageLoginComponent } from './login/pages/page-login/page-login.component';
+import { PageListOrdersComponent } from './orders/pages/page-list-orders/page-list-orders.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', component: PageLoginComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: PageLoginComponent },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule),
+  },
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,18 +7,20 @@ import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TextModule } from '../text/text.module';
 import { RouterModule } from '@angular/router';
+import { LoginModule } from '../login/login.module';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, NavComponent, FooterComponent],
-  exports: [UiModule, IconModule, TextModule, HeaderComponent, NavComponent, FooterComponent],
+  exports: [LoginModule, UiModule, IconModule, TextModule, HeaderComponent, NavComponent, FooterComponent],
   imports: [
     CommonModule,
     UiModule,
     IconModule,
     TextModule,
-    RouterModule
+    RouterModule,
+    LoginModule
   ]
 })
 export class CoreModule { }
